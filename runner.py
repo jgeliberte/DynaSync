d1d7c2d268c5707031cd9dc4c99adae662c389e9
John Geliberte <gelibertejohn@gmail.com>
2020-11-05 00:14:43 +0800

    Building structure

---
+++ runner.py
@@ -0,0 +1,24 @@
+import os
+import time
+import sys
+import configparser
+from datetime import datetime as dt
+import argparse
+from pprint import pprint
+import traceback
+
+
+class Threader:
+
+    def __init__(self):
+        print("initialize script")
+
+class Syncer:
+
+    def __init__(self):
+        print("Syncer")
+
+if __name__ == "__main__":
+
+    thread = Threader()
+    Syncer = Syncer()
