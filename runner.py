80284609ed7fe1230e7521d228425b0ea48cad68
John Geliberte <gelibertejohn@gmail.com>
2020-11-05 00:35:19 +0800

    + Initial structure

--- runner.py
+++ runner.py
@@ -7,6 +7,20 @@ import argparse
 from pprint import pprint
 import traceback
 
+class Args:
+    def get_args(self):
+        parser = argparse.ArgumentParser(
+            description="Database Syncer for CBEWS-L Softwares [-options]")
+        parser.add_argument("-s", "--server",
+                            help="smsinbox table (loggers or users)")
+        try:
+            args = parser.parse_args()
+            return args
+        except IndexError:
+            print('>> Error in parsing arguments')
+            error = parser.format_help()
+            print(error)
+            sys.exit()
 
 class Threader:
 
@@ -18,7 +32,14 @@ class Syncer:
     def __init__(self):
         print("Syncer")
 
+class DBManager:
+    def __init__(self):
+        print("Database Manager")
+
 if __name__ == "__main__":
 
+    init = Args()
     thread = Threader()
-    Syncer = Syncer()
+    syncer = Syncer()
+    dbmanager = DBManager()
+    print("Script Args:", init.get_args())
