961ad5466f0f4b124d89c9faf62d41bc51c4a4f3
John Geliberte <gelibertejohn@gmail.com>
2020-11-07 01:23:50 +0800

    + Added insert sync function

--- dbmanager.py
+++ dbmanager.py
@@ -12,15 +12,20 @@ class DatabaseCredentials():
 class DBManager:
 
 	db_cred = None
+	ops_db_cred = None
 	schemas = None
+	deployed_server = None
 
 	def __init__(self, server):
 		print(">> Initializing Database Manager")
 		credentials = DatabaseCredentials()
+		self.deployed_server = server
 		if server == "pi":
 			self.db_cred = credentials['RPI_CREDENTIALS']
+			self.ops_db_cred = credentials['CLOUD_CREDENTIALS']
 		else:
 			self.db_cred = credentials['CLOUD_CREDENTIALS']
+			self.ops_db_cred = credentials['RPI_CREDENTIALS']
 		self.schemas = credentials['DB_SCHEMAS']['schemas'].split(", ")
 
 	def getTables(self):
@@ -82,17 +87,89 @@ class DBManager:
 					return -1
 				db, cur = self.db_connect(database)
 				res = cur.execute(query)
-				print("RESULT:", res)
 				db.close()
 
 	def fetchHistory(self):
 		print(">> Fetching database history")
+		try:
+			history_list = {}
+			args = ""
 
-	def fetchHistoryReferenceData(self):
-		print(">> Fetching actual data")
+			if self.deployed_server == "pi":
+				args = "sync_status_cloud = False"
+			elif self.deployed_server == "cloud":
+				args = "sync_status_local = False"
 
-	def applyHistoryChanges(self):
+			db, cur = self.db_connect('history_collections')
+
+			query = f"SELECT * FROM history WHERE {args}"
+			res = cur.execute(query)
+			out = cur.fetchall()
+			if res:
+				for row in out:
+					history_list[row[0]] = row
+			db.close()
+			return history_list
+		except MySQLdb.OperationalError as err:
+			print(">> Error:", err)
+
+	def fetchHistoryReferenceData(self, id, data):
+		print(f">> Fetching actual data for history ID: {id}")
+		try:
+			history_data = None
+			args = ""
+			db, cur = self.db_connect(data[4])
+
+			query = f"SELECT * FROM {data[4]}.{data[3]} WHERE id={data[1]}"
+			res = cur.execute(query)
+			out = cur.fetchall()
+			if res:
+				history_data = out[0]
+			db.close()
+			return history_data
+		except MySQLdb.OperationalError as err:
+			print(">> Error:", err)
+
+
+	def applyHistoryChanges(self, data, schema, table, command):
 		print("<< Applying changes")
+		status = None
+		if command == "INSERT":
+			counter = 0
+			value_arg = "VALUES("
+			for value in data:
+				if counter != 0:
+					value_arg = value_arg + f"'{value}',"
+				else:
+					value_arg = value_arg + "0,"
+					counter = counter+1
+
+			args = f"{value_arg[:-1]})"
+			query = f"INSERT INTO {schema}.{table} {args}"
+			try:
+				db, cur = self.ops_db_connect(schema)
+				execute = cur.execute(query)
+				if execute == True:
+					status = True
+				else:
+					status = False
+				db.commit()
+			except MySQLdb.OperationalError as err:
+				print(err)
+				status = False
+			finally:
+				db.close()
+				return status
+		elif command == "UPDATE":
+			print("UPDATE DATA")
+		elif command == "DELETE":
+			print("DELETE DATA")
+		else:
+			print("INVALID")
+
+	def confirmSyncing(self, history_id, deployed_server):
+		
+
 
 	def db_connect(self, schema):
 		try:
@@ -111,5 +188,22 @@ class DBManager:
 			return False
 		except (MySQLdb.Error, MySQLdb.Warning) as err:
 			self.error_logger.store_error_log(self.exception_to_string(err))
+			print("MySQL Error:", err)
+			return False
+
+	def ops_db_connect(self, schema):
+		try:
+			db = MySQLdb.connect(self.ops_db_cred['host'],
+								 self.ops_db_cred['user'],
+								 self.ops_db_cred['password'], schema)
+			cur = db.cursor()
+			return db, cur
+		except TypeError as err:
+			print('Error Connection Value')
+			return False
+		except MySQLdb.OperationalError as err:
+			print("MySQL Operationial Error:", err)
+			return False
+		except (MySQLdb.Error, MySQLdb.Warning) as err:
 			print("MySQL Error:", err)
 			return False
--- runner.py
+++ runner.py
@@ -26,18 +26,32 @@ class Args:
             sys.exit()
 
 class Syncer:
-    def __init__(self):
-        print("Syncer")
+    def __init__(self, dbmanager, deployed_server):
+        while(True):
+            logs = dbmanager.fetchHistory()
+            for log in logs:
+                data = logs[log]
+                history_data = dbmanager.fetchHistoryReferenceData(log, data)
+                sync_status = dbmanager.applyHistoryChanges(history_data, data[4], data[3], data[2])
+                if (sync_status == True):
+                    dbmanager.confirmSyncing(data[0], deployed_server)
+                sys.exit(0)
+
 
 if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read('config/config.cnf')
     init = Args()
     deployed_server = init.get_args()
-    # thread = Threader(config['SYNC_CONFIG']['THREAD_COUNT'])
-    # syncer = Syncer()
     dbmanager = db.DBManager(deployed_server.server)
     db_tables = dbmanager.getTables()
     for database in db_tables:
         status = dbmanager.checkTriggers(database, db_tables[database])
+
+    print("\n\n>>-----------------------------------------")
+    print(">>              Triggers set")
+    print(">>-----------------------------------------\n\n")
+
+    syncer = Syncer(dbmanager, deployed_server.server)
+
     # print("Script Args:", init.get_args())
